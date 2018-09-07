d89ab8c3027f1b2c029d44f95bdc1e45be0dbbab
gabrielcoder <gabrielcoder247@gmail.com>
2018-09-07 15:05:39 +0300

    create app for manage.py file

--- manage.py
+++ manage.py
@@ -1,1 +1,13 @@
 from flask_script import Manager,Server
+
+
+app = create_app('development')
+
+manager = manager(app)
+manager.add_command('server',Server)
+
+
+
+if __name__ == '__main__':
+    manager.run()
+
