b7f738b88de7a31ba82aeea20aa28b191386d16f
gabrielcoder <gabrielcoder247@gmail.com>
2018-09-07 17:52:29 +0300

    install sqlalchemy and psycopg2- driver

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from config import config_options
 
 def create_app(config_name):
--- config.py
+++ config.py
@@ -2,7 +2,7 @@ class Config:
     '''
     General configuration parent class
     '''
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://username:password@localhost/pitches'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://gabrielcoder:pitch2018@localhost/pitch'
 
 
 
