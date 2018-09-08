b5e039dd2676c4108600331a5b2ed5a65284e3d7
gabrielcoder <gabrielcoder247@gmail.com>
2018-09-08 09:48:49 +0300

    create function for test discover in manage.py

--- manage.py
+++ manage.py
@@ -15,6 +15,12 @@ manager.add_command('db',MigrateCommand)
 def make_shell_context():
     return dict(app = app,db = db, User = User)
 
+def test():
+    # Run the unit test   
+    import unittest
+     tests = unittest.TestLoader().discover('tests')
+     unittest.TextTestRunner(verbosity=2).run(tests)
+
 if __name__ == '__main__':
     manager.run()
 
--- tests/test_user.py
+++ tests/test_user.py
@@ -10,3 +10,9 @@ class UserModelTest(unittest.TestCase):
     def test_password_setter(self):
         self.assertTrue(self.new_user.pass_secure is not None)
 
+
+    def test_no_access_password(self):
+        with self.assertRaises(AtrributeError): 
+            self.new_user.password
+    def test_password_verification(self):           
+
