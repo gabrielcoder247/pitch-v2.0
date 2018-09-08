2bd064f9f6ac480e0abde98a81a217805211c413
gabrielcoder <gabrielcoder247@gmail.com>
2018-09-08 15:57:52 +0300

    create forms for auth forms

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -11,3 +11,11 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('password',validators = [Required(),EqualTo('password_confirm',message = 'passwords must match')])
     password_confirm = passwordField('Confirm passwords' validators = [Required()])
     submit = SubmitField('Sign Up')
+
+    def validate_email(self,data_field):
+        if User.query.filter_by(email = data_field.data).first():
+            raise ValidationError('There is an account with that email')
+
+    def validate_username(self,data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError('That username is Taken') 
