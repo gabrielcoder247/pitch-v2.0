a3291b18c4b54191c4503a6d3301ee8c4af6464f
gabrielcoder <gabrielcoder247@gmail.com>
2018-09-08 15:16:02 +0300

    create models for category

--- app/models.py
+++ app/models.py
@@ -111,3 +111,22 @@ class Comment(db.Model):
         return comments
 
 
+
+class Category(db.Model):
+    '''
+    Function that defines different categories of pitches
+    '''
+    __tablename__ ='categories'
+
+
+    id = db.Column(db.Integer, primary_key=True)
+    name_of_category = db.Column(db.String(255))
+    category_description = db.Column(db.String(255))
+
+    @classmethod
+    def get_categories(cls):
+        '''
+        This function fetches all the categories from the database
+        '''
+        categories = Category.query.all()
+        return categories
