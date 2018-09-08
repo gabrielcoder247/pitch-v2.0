13a39f8cf75a278cf6a85b113c4b64803b30a620
gabrielcoder <gabrielcoder247@gmail.com>
2018-09-08 08:08:41 +0300

    install ogin manager, instantiate and create methods for password hashing

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 from flask_script import Manager,Server
-from flask_migrate import Migrate,MigrateCommmand
+from flask_migrate import Migrate,MigrateCommand
 from app.models import User
 from app import create_app,db
 
