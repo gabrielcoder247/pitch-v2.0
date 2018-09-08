074f7e8996f72708560d06b8a7e32b60b4554178
gabrielcoder <gabrielcoder247@gmail.com>
2018-09-08 08:52:53 +0300

    test password setter

--- tests/test_user.py
+++ tests/test_user.py
@@ -6,4 +6,7 @@ from app.models import User
 class UserModelTest(unittest.TestCase):
     def setUp(self):
         self.new_user = User(password = 'moringa')
-        
+
+    def test_password_setter(self):
+        self.assertTrue(self.new_user.pass_secure is not None)
+
