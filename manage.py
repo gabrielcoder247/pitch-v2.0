6297d8d9a94a67fc49bf8904ff9ca82119db5287
gabrielcoder <gabrielcoder247@gmail.com>
2018-09-08 14:15:09 +0300

    import login required to main views

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@ from flask_script import Manager,Server
 from flask_migrate import Migrate,MigrateCommand
 from app.models import User
 from app import create_app,db
+import unittest
 
 app = create_app('development')
 
@@ -15,12 +16,13 @@ manager.add_command('db',MigrateCommand)
 def make_shell_context():
     return dict(app = app,db = db, User = User)
 
+@manager.command
 def test():
     # Run the unit test   
-        import unittest
+       
     
-        tests= unittest.TestLoader().discover('tests')
-        unittest.TextTestRunner(verbosity=2).run(tests)
+    tests= unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
 if __name__ == '__main__':
     manager.run()
--- tests/test_user.py
+++ tests/test_user.py
@@ -4,15 +4,18 @@ from app.models import User
 
 
 class UserModelTest(unittest.TestCase):
+
     def setUp(self):
-        self.new_user = User(password = 'moringa')
+        self.new_user = User(password = 'banana')
 
     def test_password_setter(self):
         self.assertTrue(self.new_user.pass_secure is not None)
 
 
     def test_no_access_password(self):
-        with self.assertRaises(AtrributeError): 
+        with self.assertRaises(AttributeError): 
             self.new_user.password
-    def test_password_verification(self):           
+
+    def test_password_verification(self):
+        self.assertTrue(self.new_user.verify_password('banana'))           
 
