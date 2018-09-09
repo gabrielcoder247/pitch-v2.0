1f9c18744ef1e71916f03731530c46836436fb08
gabrielcoder <gabrielcoder247@gmail.com>
2018-09-09 08:38:32 +0300

    create route for registration in the auth

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,8 +4,17 @@ from flask_login import login_user
 from ..models import User
 from .forms import LoginForm,RegistrationForm
 
-#....
 
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)
 
 
 @auth.route('/login',methods=['GET','POST'])
