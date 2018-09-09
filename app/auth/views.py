d190dd0edab1b41366e1f2457c4d390a569e6035
gabrielcoder <gabrielcoder247@gmail.com>
2018-09-09 09:44:08 +0300

    create navbar

--- app/auth/views.py
+++ app/auth/views.py
@@ -5,18 +5,6 @@ from ..models import User
 from .forms import LoginForm,RegistrationForm
 
 
-@auth.route('/register',methods = ["GET","POST"])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        return redirect(url_for('auth.login'))
-        title = "New Account"
-    return render_template('auth/register.html',registration_form = form)
-
-
 @auth.route('/login',methods=['GET','POST'])
 def login():
     login_form = LoginForm()
@@ -30,4 +18,19 @@ def login():
 
     title = "Pitch login"
     return render_template('auth/login.html',login_form = login_form,title=title)
+
+
+
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)
+
+
 
