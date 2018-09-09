018e7c90364a6a941809fc2facf1d6853d89bc77
gabrielcoder <gabrielcoder247@gmail.com>
2018-09-09 12:43:40 +0300

    create profile template

--- app/main/views.py
+++ app/main/views.py
@@ -14,3 +14,12 @@ def index():
 # @main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
 # @login_required
 # def new_review(id):
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
