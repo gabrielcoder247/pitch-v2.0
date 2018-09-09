28c0cd6f19be820ed905450a9ea3f9f61b163f90
gabrielcoder <gabrielcoder247@gmail.com>
2018-09-09 12:22:28 +0300

    add navbar datas from the views functions

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,15 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, passwordField,SubmitField
+from wtforms import StringField, PasswordField,SubmitField,BooleanField
 from wtforms.validators import Required, Email, EqualTo
 from  ..models import User
 from wtforms import ValidationError
 
 
 class RegistrationForm(FlaskForm):
-    email = StringField('Your Email Address',validators[Required(),Email()])
+    email = StringField('Your Email Address',validators = [Required(),Email()])
     username = StringField('Enter your username',validators = [Required()])
     password = PasswordField('password',validators = [Required(),EqualTo('password_confirm',message = 'passwords must match')])
-    password_confirm = passwordField('Confirm passwords' validators = [Required()])
+    password_confirm = PasswordField('Confirm passwords', validators = [Required()])
     submit = SubmitField('Sign Up')
 
     def validate_email(self,data_field):
@@ -19,3 +19,9 @@ class RegistrationForm(FlaskForm):
     def validate_username(self,data_field):
         if User.query.filter_by(username = data_field.data).first():
             raise ValidationError('That username is Taken') 
+
+class LoginForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    password = PasswordField('Password',validators =[Required()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')            
