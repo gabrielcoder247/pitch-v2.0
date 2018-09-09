825c993b847fc4000905444a6ec25aa7168282d6
gabrielcoder <gabrielcoder247@gmail.com>
2018-09-09 14:04:55 +0300

    install flask-mail

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import UploadSet,configure_uploads,IMAGES
-# from flask_mail import Mail
+from flask_mail import Mail
 # from flask_simplemde import SimpleMDE
 
 
@@ -15,7 +15,7 @@ login_manager.login_view = 'auth.login'
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 photos = UploadSet('photos', IMAGES)
-# mail = Mail()
+mail = Mail()
 # simple = SimpleMDE()
 
 
@@ -30,7 +30,7 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
-    # mail.init_app(app)
+    mail.init_app(app)
     # simple.init_app(app)
 
      # Registering the blueprint
