44f31e442da08a2146def96e011559d56e5f34be
gabrielcoder <gabrielcoder247@gmail.com>
2018-09-14 13:50:40 +0300

    deploying to heroku

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from flask_script import Manager,Server
 from flask_migrate import Migrate,MigrateCommand
-from app.models import User,Pitch,Category
+from app.models import User,Pitch,Category, Comment
 from app import create_app,db
 import unittest
 
