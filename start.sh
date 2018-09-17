743b537fa44d3e885bd13de5db9322a5f69d449f
gabrielcoder <gabrielcoder247@gmail.com>
2018-09-17 10:35:57 +0300

    deploying to heroku

--- start.sh
+++ start.sh
@@ -1,12 +1,15 @@
 
-MAIL_SERVER = 'smtp.googlemail.com'
-MAIL_PORT = 587
-MAIL_USE_TLS = True
-MAIL_USERNAME = 'gabrielcoder247@gmail.com'
-MAIL_PASSWORD = 'dushanbe2015'
-SUBJECT_PREFIX = 'pitchit'
-SENDER_EMAIL = 'gabrielcoder247@gmail.com'
-sender='gabrielcoder247@gmail.com'
+# MAIL_SERVER = 'smtp.googlemail.com'
+# MAIL_PORT = 587
+# MAIL_USE_TLS = True
+# MAIL_USERNAME = 'gabrielcoder247@gmail.com'
+# MAIL_PASSWORD = 'dushanbe2015'
+# SUBJECT_PREFIX = 'pitchit'
+# SENDER_EMAIL = 'gabrielcoder247@gmail.com'
+# sender='gabrielcoder247@gmail.com'
+export MAIL_USERNAME='gabrielcoder247@gmail.com'
+export MAIL_PASSWORD='@#dushanbe2015'
+
 
 export DATABASE_URL='postgres://ardywwnajqlzjp:28b9f1d95cf228f87f501fb5605739a02bf5f7c7e58fce9087d8a31cd6f7a38d@ec2-23-23-253-106.compute-1.amazonaws.com:5432/dfd0o7m0u0qe1u'
 
